5f80c32200609c4961d989488c486b74562a7a46
Simeon Parvanov <simeonparvanov@ymail.com>
2015-10-24 15:02:24 +0300

    Points final version

---
+++ First application problem/Points/Directions.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points
+{
+
+    class Directions
+    {
+        private int x;
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+        public int y;
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+
+        public int[] initialPosition = new int[2];
+
+        public Directions(int firstParam, int secondParam, int[] arr)
+        {
+            this.x = firstParam;
+            this.y = secondParam;
+            this.initialPosition = arr;
+        }
+        public void AddToArray()
+        {
+            initialPosition[0] = X;
+            initialPosition[1] = Y;
+        }
+        public int[] ReactOnWarp(string inputSymbolString)
+        {
+            char[] test = inputSymbolString.ToCharArray();
+            bool changeDirection = false;
+
+            for (int i = 0; i <= test.Length-1; i++)
+            {
+                if (test[i] == '~')
+                {
+                    //swap
+                    changeDirection = !changeDirection;
+                }
+                if (changeDirection)
+                {
+                    MoveInverse(test[i]);
+                    AddToArray();
+                }
+                else
+                {
+                    MoveNormal(test[i]);
+                    AddToArray();
+                }
+            }
+            return initialPosition;
+        }
+
+        public void MoveNormal(char symbol)
+        {
+            switch (symbol)
+            {
+                //'>>><<<~>>>~^^^'
+                case '>':
+                    X++;
+                    break;
+                case '<':
+                    X--;
+                    break;
+                case 'v':
+                    Y++;
+                    break;
+                case '^':
+                    Y--;
+                    break;
+            }
+        }
+        public void MoveInverse(char symbol)
+        {
+            switch (symbol)
+            {
+                //'>>><<<~>>>~^^^'
+                case '<':
+                    X++;
+                    break;
+                case '>':
+                    X--;
+                    break;
+                case 'v':
+                    Y--;
+                    break;
+                case '^':
+                    Y++;
+                    break;
+
+            }
+        }
+    }
+}
--- First application problem/Points/Program.cs
+++ First application problem/Points/Program.cs
@@ -8,8 +8,21 @@ namespace Points
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+            Console.WriteLine("Pick two points");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            int[] arr = { x, y };
+            Directions Direction = new Directions(x, y, arr);
+            Console.WriteLine("Enter directions");
+            string directions = Console.ReadLine();
+
+            int[] resultArr = Direction.ReactOnWarp(directions);
+           
+            Console.Write(resultArr[0] + ", "+ resultArr[1]);
         }
     }
 }
