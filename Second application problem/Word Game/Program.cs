2587c3b604d343826ee4d280216e6bae5b1a20be
Simeon Parvanov <simeonparvanov@ymail.com>
2015-10-31 11:06:30 +0200

    Finished second application problem, added directions and match logic

--- Second application problem/Word Game/Program.cs
+++ Second application problem/Word Game/Program.cs
@@ -9,12 +9,98 @@ namespace Word_Game
     public class Program
     {
         public static int counter = 0;
-        public static Reader ReaderFunc = new Reader(counter);//the counter will count the occurences of the word in the matrix
         public static string name = "ivan";
+
         static void Main(string[] args)
         {
+            Reader ReaderFunc = new Reader();//the counter will count the occurences of the word in the matrix
+
             char[,] twoDResult = ReaderFunc.ReadCharsFromFile();
-            ReaderFunc.SearchWord(twoDResult, name);
+            Console.WriteLine(CountOccurrences(twoDResult, name));
+        }
+        public static int CountOccurrences(char[,] table, string word)
+        {
+            //Always ceck if the string is empty.
+            //The table is checked if empty in the reader method
+            if (string.IsNullOrEmpty(word)) return 0;
+            int count = 0;
+            //count rows and columns.
+            int rowCount = table.GetLength(0);
+            int colCount = table.GetLength(1);
+            //traverse the matrix
+            for (int row = 0; row < rowCount; row++)
+            {
+                for (int col = 0; col < colCount; col++)
+                {
+                    //look for the first letter of the string and if found jump over the next iteration
+                    if (table[row, col] != word[0])
+                    {
+                        continue;
+                    }
+                    if (word.Length == 1)
+                    {
+                        count++;
+                        continue;
+                    }
+                    //go in all 8 diretions
+                    for (int directionY = -1; directionY <= 1; directionY++)
+                    {
+                        for (int directionX = -1; directionX <= 1; directionX++)
+                        {       //if both zero there is no direction
+                            if ((directionX != 0 || directionY != 0) && Match(word, table, rowCount, colCount, row, col, directionY, directionX))
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+        private static bool Match(string word, char[,] table, int rowCount, int colCount, int startRow, int startCol, int directionY, int directionX)
+        {
+            int row = startRow + directionY;
+            int storeDirectionY = directionY;
+          
+            if (directionY < 0)
+            {
+                storeDirectionY = row;
+            }
+            else
+            {
+                storeDirectionY = rowCount - row;
+            }
+
+            if (directionY != 0 && storeDirectionY < word.Length - 1)
+            {
+                return false;
+            }
+            //copy logic for horizontal search
+            int storeDirectionX = directionX;
+            int col = startCol + directionX;
+           
+            if (directionX < 0)
+            {
+                storeDirectionX = col;
+            }
+            else
+            {
+                storeDirectionX = colCount - col;
+            }
+
+            if (directionX != 0 && storeDirectionX < word.Length - 1)
+            {
+                return false;
+            }
+
+            for (int charPos = 1; charPos < word.Length; row += directionY, col += directionX, charPos++)
+            {
+                if (table[row, col] != word[charPos])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
--- Second application problem/Word Game/Reader.cs
+++ Second application problem/Word Game/Reader.cs
@@ -9,15 +9,8 @@ namespace Word_Game
 {
     public class Reader
     {
-        public int[,] startingPosition = new int[1, 1];
-        public int counter = 0;
-        public Reader(int counter)
-        {
-            this.counter = counter;
-        }
-
-        public static char[,] results = new char[4, 5];
-        public static char[,] ReadCharsFromFile()
+        public char[,] results = new char[4, 5];
+        public char[,] ReadCharsFromFile()
         {
             try
             {
@@ -54,44 +47,5 @@ namespace Word_Game
             }
             return results;
         }
-        public void SearchWord(char[,] input, string name)
-        {
-            //1. loop through the array and look for the first letter of the string
-            //2. if found search in all directions "iterative"
-            //3. if one direction doesn't find it break out of the mathod and continue to search
-            //4. if found mark the positions so you don't find the same word more than once
-            char firstLetter = name[0];
-
-            //go look for it in the 2d array
-            for (int y = 0; y < 5; y++)
-            {
-                for (int x = 0; x < 4; x++)
-                {
-                    if (results[x, y] == firstLetter)//found the letter
-                    {
-                        Console.WriteLine("Found it " +" "+ firstLetter);
-                        SearchRightDirection(results, x, y);
-                    }
-                }
-            }
-            //if you found the second letter, look only one step at a time in the same direction
-
-            //if found start searching in all possible directionsif (i >= width ||
-        }
-
-        public bool SearchRightDirection(char [,] matrix, int x, int y)
-        {
-            
-            for (int columns = 0; columns < matrix.Length; columns++)
-            {
-                for (int rows = 0; rows < length; rows++)
-                {
-
-                }
-            }
-            return false;
-        }
     }
 }
-
-
